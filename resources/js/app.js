26d0bc3caae7215428e4ebd32fc6364a74851895
chirolist <document.top.value@gmail.com>
2021-01-10 20:12:46 +0900

    commit

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,6 +6,9 @@
 
 require('./bootstrap');
 
+import App from './app.vue'
+//import { event } from './utils'
+
 window.Vue = require('vue');
 
 /**
@@ -19,8 +22,6 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
@@ -28,5 +29,9 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  */
 
 const app = new Vue({
-    el: '#app',
+  el: '#app',
+  render: h => h(App),
+  created () {
+    //event.init()
+  }
 });
